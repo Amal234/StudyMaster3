ada70480a317122a939061d83472597409762a34
amal <amal.alaswad@icloud.com>
2020-07-01 19:36:19 +0200

    buttons binding

--- app/src/main/java/com/example/studymaster/week_days_Fragment.kt
+++ app/src/main/java/com/example/studymaster/week_days_Fragment.kt
@@ -26,9 +26,31 @@ class week_days_Fragment : Fragment() {
 
         binding.buttonStats.setOnClickListener (
             Navigation.createNavigateOnClickListener(R.id.action_week_days_Fragment_to_subject_Statistics_Fragement)
-
         )
-
+        binding.buttonMo.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_week_days_Fragment_to_monday_fragment)
+        )
+        binding.buttonDi.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_week_days_Fragment_to_tuesday_fragment)
+        )
+        binding.buttonMi.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_week_days_Fragment_to_wednesday_fragment)
+        )
+        binding.buttonDo.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_week_days_Fragment_to_thursday_fragment)
+        )
+        binding.buttonFr.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_week_days_Fragment_to_friday_fragment)
+        )
+        binding.buttonSa.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_week_days_Fragment_to_saturday_fragment)
+        )
+        binding.buttonSo.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_week_days_Fragment_to_sunday_fragment)
+        )
+        binding.buttonStats.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_week_days_Fragment_to_subject_Statistics_Fragement)
+        )
 
         return binding.root
     }
