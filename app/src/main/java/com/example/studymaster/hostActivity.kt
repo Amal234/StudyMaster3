2e8cbf293f8d5c2d783046a98a99175aafd0ff59
MUensal <mehtap.uensal.fh@gmail.com>
2020-07-01 21:33:04 +0200

    Toolbar and back(up)button working. TextView added to weekdaysfragment

--- app/src/main/java/com/example/studymaster/hostActivity.kt
+++ app/src/main/java/com/example/studymaster/hostActivity.kt
@@ -2,11 +2,18 @@ package com.example.studymaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class hostActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host)
+
+        //TODO
+        val toolbar = findViewById(R.id.toolbar) as Toolbar?
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
