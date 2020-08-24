d1e58f175e6e8c5ace7e72aab7153d6935918c1a
amal <amal.alaswad@icloud.com>
2020-08-24 19:06:18 +0200

    DO, FR fragment with the functions

--- app/src/main/java/com/example/studymaster/saturday_fragment.kt
+++ app/src/main/java/com/example/studymaster/saturday_fragment.kt
@@ -1,5 +1,7 @@
 package com.example.studymaster
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,18 +10,38 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.studymaster.databinding.SaturdayBinding
+import kotlinx.android.synthetic.main.monday.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class saturday_fragment : Fragment() {
-
+    val myarrayList = ArrayList<String>()//Array for user input Subject
+    val arrayTime = ArrayList<String>()
+    @SuppressLint("ServiceCast")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding: SaturdayBinding = DataBindingUtil.inflate(
             inflater, R.layout.saturday, container, false)
+        val addButton = binding.addButton
+        val subjectList= binding.mylist
+        val timeList = binding.mylist
+        //val timeSpinner= binding.TimeSpinner
+
+        val adapter = MyCustomAdapter(myarrayList, arrayTime, activity as Context)
+
+
+        addButton.setOnClickListener {
+            val subject = editText.text
+            val time = TimeSpinner.selectedItem
+            myarrayList.add(subject.toString())
+            arrayTime.add(time.toString())
+
+            subjectList.adapter = adapter
+            timeList.adapter = adapter
+        }
         return binding.root
         }
     }
